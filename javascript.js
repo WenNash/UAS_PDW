3744b4941273adc976fff1882e0ae0c0d47b8001
WenNash <weannashrullah@gmail.com>
2023-07-08 19:48:10 +0700

    UAS Praktikum PDW

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// Method 1
+function greet() {
+  console.log("Hello, World!");
+}
+
+// Method 2
+function calculateSum(a, b) {
+  return a + b;
+}
+
+// Method 3
+function toggleVisibility() {
+  var element = document.getElementById("toggleElement");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
